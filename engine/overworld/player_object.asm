66904919e9ed1c94c484ce6d55476187c3be5571
ELgazo110 <gary.wyllie110@outlook.com>
2024-11-24 11:41:05 +0000

    colour test

--- engine/overworld/player_object.asm
+++ engine/overworld/player_object.asm
@@ -31,8 +31,11 @@ SpawnPlayer:
 	ld hl, MAPOBJECT_PALETTE
 	add hl, bc
 	ld a, [wPlayerGender]
-	lb de, PAL_NPC_RED, OBJECTTYPE_SCRIPT
+	lb de, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT
 	and a ; PLAYER_MALE
+	jr z, .ok
+	lb de, PAL_NPC_RED, OBJECTTYPE_SCRIPT
+	and a ;
 	jr z, .ok
 	assert PAL_NPC_RED + 1 == PAL_NPC_BLUE
 	inc d
